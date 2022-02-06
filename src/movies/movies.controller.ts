6910363e01d652b4c73cc8846a982f43bbfa1d7e
satoshi25 <satoshi25.kr@gmail.com>
2022-02-07 01:10:20 +0900

    Modules and Dependency Injection

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -12,10 +13,10 @@ export class MoviesController {
     return this.moviesService.getAll();
   }
 
-  @Get('search')
-  search(@Query('year') searchingYear: string) {
-    return `We are searching for a movie made after: ${searchingYear} `
-  }
+  // @Get('search')
+  // search(@Query('year') searchingYear: string) {
+  //   return `We are searching for a movie made after: ${searchingYear} `
+  // }
 
   @Get(":id")
   getOne(@Param("id") movieId: number): Movie {
@@ -33,7 +34,7 @@ export class MoviesController {
   }
 
   @Patch(':id')
-  patch(@Param('id') movieId: number, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.update(movieId, updateData);
   }
 
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,11 +11,11 @@ export class MoviesService {
     return this.movies;
   }
 
-  getOne(id: number):Movie {
-    const movie = this.movies.find(movie => movie.id === id)
+  getOne(id: number): Movie {
+    const movie = this.movies.find(movie => movie.id === +id);
     // return this.movies.find(movie => movie.id === +id)
     if (!movie) {
-      throw new NotFoundException(`Movie with ID ${id} Not Found`)
+      throw new NotFoundException(`Movie with ID ${id} Not Found.`);
     }
     return movie; 
   }
@@ -27,11 +28,11 @@ export class MoviesService {
   create(movieData: CreateMovieDto) {
     this.movies.push({
       id: this.movies.length + 1,
-      ...movieData
+      ...movieData,
     })
   }
 
-  update(id: number, updateData) {
+  update(id: number, updateData: UpdateMovieDto) {
     const movie = this.getOne(id);
     this.deleteOne(id);
     this.movies.push({ ...movie, ...updateData });
