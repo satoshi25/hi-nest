f58d6678e260017770635e34332875d74f6043c0
satoshi25 <satoshi25.kr@gmail.com>
2022-02-06 21:22:21 +0900

    Movies service part Two

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,13 +10,17 @@ export class MoviesService {
   }
 
   getOne(id: string):Movie {
-    return this.movies.find(movie => movie.id === parseInt(id))
+    const movie = this.movies.find(movie => movie.id === parseInt(id))
     // return this.movies.find(movie => movie.id === +id)
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID ${id} Not Found`)
+    }
+    return movie; 
   }
 
-  deleteOne(id: string):boolean {
-    this.movies.filter(movie => movie.id !== +id);
-    return true;
+  deleteOne(id: string) {
+    this.getOne(id)
+    this.movies = this.movies.filter(movie => movie.id !== +id);
   }
 
   create(movieData) {
@@ -24,6 +28,12 @@ export class MoviesService {
       id: this.movies.length + 1,
       ...movieData
     })
+  }
+
+  update(id: string, updateData) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...updateData });
   }
 
 }
